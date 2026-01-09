4be44ef299b6fbdf67a7f8bf13de39c4c66a94d0
Yusang-1 <206505633+Yusang-1@users.noreply.github.com>
2026-01-09 11:29:22 +0900

    Feat : 스테이지 SO 생성

---
+++ Assets/Scripts/Battle/Stage/SO/StageSO.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System;
+
+[CreateAssetMenu(fileName = "StageSO", menuName = "Scriptable Objects/StageSO")]
+public class StageSO : ScriptableObject
+{
+    [SerializeField] private int stageNum;
+    [SerializeField] private RoundData roundData;
+
+    public int StageNum => stageNum;
+    public RoundData RoundData => roundData;
+}
+
+[Serializable]
+public struct RoundData
+{
+    [SerializeField] private int enemyCode;
+    [SerializeField] private int enemyCount;
+    [SerializeField] private int time;
+
+    public int EnemyCode => enemyCode;
+    public int EnemyCount => enemyCount;
+    public int Time => time;
+}
