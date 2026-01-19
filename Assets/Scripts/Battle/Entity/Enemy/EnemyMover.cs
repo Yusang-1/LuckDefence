37e9d090847c84f939eeab14387a84f4ba4aab1f
Yusang-1 <206505633+Yusang-1@users.noreply.github.com>
2026-01-19 15:46:34 +0900

    Refactor : Mover 클래스 추상화

---
+++ Assets/Scripts/Battle/Entity/Enemy/EnemyMover.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class EnemyMover : EntityMover
+{
+    private int currentBeaconIndex;
+
+    private void Update()
+    {
+        Move();
+    }
+
+    public override void Initialize(Entity entity)
+    {
+        base.Initialize(entity);
+
+        directionVector = GetDestinationVector();
+    }
+
+    protected override Vector3 GetDestinationVector()
+    {
+        transform.position = BeaconContainer.s_Beacons[currentBeaconIndex].position;
+
+        currentBeaconIndex++;
+        if (currentBeaconIndex >= BeaconContainer.s_Beacons.Length)
+        {
+            currentBeaconIndex = 0;
+        }
+
+        return (BeaconContainer.s_Beacons[currentBeaconIndex].position - transform.position).normalized;
+    }
+
+    protected override void Move()
+    {
+        transform.position += directionVector * entity.Data.MoveSpeed * Time.deltaTime;
+
+        if (Vector3.Distance(transform.position, BeaconContainer.s_Beacons[currentBeaconIndex].position) <= Vector3.Distance(transform.position, transform.position + directionVector * entity.Data.MoveSpeed * Time.deltaTime))
+        {
+            directionVector = GetDestinationVector();
+        }
+    }
+}
---
+++ Assets/Scripts/Battle/Entity/EntityMover.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public abstract class EntityMover : MonoBehaviour
+{
+    protected Entity entity;    
+    protected Vector3 directionVector;    
+
+    public virtual void Initialize(Entity entity)
+    {
+        this.entity = entity;        
+    }
+
+    protected abstract Vector3 GetDestinationVector();
+    
+
+    protected abstract void Move();
+}
